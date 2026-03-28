35d6e53e9d2c43fd9478b852afda115907faa66f
Roy Armony <105720927+RoyArmony@users.noreply.github.com>
2026-03-28 19:40:06 +0300

    Bump version to 0.2.0

--- src/linksnip/__init__.py
+++ src/linksnip/__init__.py
@@ -3,7 +3,7 @@
 A generic URL shortening client that works with any compatible backend API.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .client import Client
 from .exceptions import (
